bacbe97666d090afe1eae1f5d94f42fa49a0370a
TheCampagnards <konstantin.sidorenko@orange.fr>
2016-01-29 21:16:12 +0100

    html squelette jeux

--- vues/jeu.php
+++ vues/jeu.php
@@ -1,3 +1,22 @@
-<pre>
-<?php var_dump($jeux) ?>
-</pre>
+<?php $count_jeux = count($jeux);
+for ($i=0; $i < $count_jeux; $i++): ?>
+	<article id="<?php echo $i ?>" style="background-color:blue">
+		<h1><?php echo $jeux[$i]->titre ?></h1>
+		<h2><?php echo $jeux[$i]->auteur ?></h2>
+		<img src="<?php echo $jeux[$i]->image ?>" alt="description-jeu">
+		<p><?php echo $jeux[$i]->description ?></p>
+
+		<h2>N<?php $count_commentaires = count($jeux[$i]->commentaires);
+		echo $count_commentaires; ?>
+		<section>
+		<?php for ($j=0; $j < $count_commentaires; $j++) : ?>
+			<article style="background-color:green">
+				<h3><?php echo $jeux[$i]->commentaires[$j]->date ?></h3>
+				<h3><?php echo $jeux[$i]->commentaires[$j]->pseudo ?></h3>
+				<p><?php echo $jeux[$i]->commentaires[$j]->note ?></p>
+				<p><?php echo $jeux[$i]->commentaires[$j]->commentaire ?></p>
+			</article>
+		<?php endfor; ?>
+		</section>
+	</article> 
+<?php endfor; ?>
