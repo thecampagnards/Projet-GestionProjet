fb582b77478057cbbe8c09816957b6927099a287
TheCampagnards <konstantin.sidorenko@orange.fr>
2016-02-02 16:43:46 +0100

    fix issues

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 //pour la class active des urls de la barre de navigation
 var url = window.location.pathname.split("/");
 var params = url[url.length - 1];
-if(params === "")
-    params = "./";
+if(params === "") params = "./";
 $("ul.nav li").each(function(index, el) {
     if($(this).find("a").attr("href") === params){
         $(this).addClass("active");
