fbe13f83b25c4c480688eb5eae355a4cbe9baf85
TheCampagnards <konstantin.sidorenko@orange.fr>
2016-02-04 14:31:38 +0100

    ajout des liens equipe

--- vues/home.php
+++ vues/home.php
@@ -16,9 +16,9 @@
 						Je suis étudiant à l'ISEN Brest, en troisième année de Cycle Informatique et Réseaux et j'effectue cette année de scolarité en alternance au Crédit Mutuel Arkéa. Je suis passionné par l'informatique et plus particulièrement par le développement web.
 					</div>
 					<div class="liens">
-						<p><i class="fa fa-globe"></i> http://delemottedavid.fr/</p>
-						<p><i class="fa fa-facebook-official"></i> https://www.facebook.com/David.Dele</p>
-						<p><i class="fa fa-linkedin"></i> https://www.linkedin.com/in/delemottedavid</p>
+						<p><i class="fa fa-globe"></i> <a href="http://delemottedavid.fr/" target="_blank">http://delemottedavid.fr/</a></p>
+						<p><i class="fa fa-facebook-official"></i> <a href="https://www.facebook.com/David.Dele" target="_blank">https://www.facebook.com/David.Dele</a></p>
+						<p><i class="fa fa-linkedin"></i> <a href="https://www.linkedin.com/in/delemottedavid" target="_blank">https://www.linkedin.com/in/delemottedavid</a></p>
 					</div>
 				</div>
 			</div>
@@ -42,9 +42,9 @@
 						Je suis étudiant en troisième année de cycle informatique et réseaux (CIR) à l'ISEN Brest. Depuis mon enfance, je suis passionné par l'informatique, j'ai donc choisi une voie me permettant de pratiquer différents domaines de l'informatique dès la première année de formation. J'aime aussi le design, je réalise des logos, bannières, maquettes et autres pour des amis et moi-même. 
 					</div>
 					<div class="liens">
-						<p><i class="fa fa-globe"></i> https://evanguelard.fr/</p>
-						<p><i class="fa fa-facebook-official"></i> https://www.facebook.com/Evan.Guelard29</p>
-						<p><i class="fa fa-twitter"></i> https://twitter.com/Evan_Guelard</p>
+						<p><i class="fa fa-globe"></i> <a href="https://evanguelard.fr/" target="_blank">https://evanguelard.fr/</a></p>
+						<p><i class="fa fa-facebook-official"></i> <a href="https://www.facebook.com/Evan.Guelard29" target="_blank">https://www.facebook.com/Evan.Guelard29</a></p>
+						<p><i class="fa fa-twitter"></i> <a href="https://twitter.com/Evan_Guelard" target="_blank">https://twitter.com/Evan_Guelard</a></p>
 					</div>
 				</div>
 			</div>
@@ -68,9 +68,9 @@
 						Je m'appelle Konstantin et je suis étudiant en 2ème année d’école d’ingénieurs à l'ISEN de Brest en Cycle Informatique et Réseaux. Ma passion pour l'informatique a commencé au collège lorsque j'ai monté mon propre ordinateur avec l'aide de mon père. Par la suite j'ai appris par moi-même le langage C. En terminale, j'ai choisi de suivre la spécialité ISN (Informatique et Sciences du Numérique) en parallèle de la spécialité Mathématiques. Grâce à ma 1ère année de formation à l'ISEN, j'ai acquis de bonnes bases en programmation (voir mes compétences). Le sport fait aussi partie de ma vie ; je joue dans l’équipe sénior du club Quimper Communauté Hand Ball.
 					</div>
 					<div class="liens">
-						<p><i class="fa fa-globe"></i> https://konstantin-sidorenko.fr/</p>
-						<p><i class="fa fa-facebook-official"></i> https://www.facebook.com/konstantin.sidorenko.16</p>
-						<p><i class="fa fa-twitter"></i> https://twitter.com/TheCampagnards</p>
+						<p><i class="fa fa-globe"></i> <a href="https://konstantin-sidorenko.fr/" target="_blank">https://konstantin-sidorenko.fr/</a></p>
+						<p><i class="fa fa-facebook-official"></i> <a href="https://www.facebook.com/konstantin.sidorenko.16" target="_blank">https://www.facebook.com/konstantin.sidorenko.16</a></p>
+						<p><i class="fa fa-twitter"></i> <a href="https://twitter.com/TheCampagnards" target="_blank">https://twitter.com/TheCampagnards</a></p>
 					</div>
 				</div>
 			</div>
